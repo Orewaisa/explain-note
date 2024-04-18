1323d643e7ec01d7f7063595173faedf223f52d0
step <testerbot1525@gmail.com>
2024-04-18 16:43:07 +0300

    fix

--- app.vue
+++ app.vue
@@ -1,24 +1,13 @@
 <template>
   <head>
-    <title>Объяснительная</title>
+    <title>Объяснительная записка</title>
   </head>
 
   <main>
-    <Header />
-    <h1 class="ubuntu-regular">Объяснительная записка</h1>
-
     <FormNote />
   </main>
 </template>
 
 <style lang="less">
   @import "@/assets/styles/main.less";
-  h1 {
-    text-align: center;
-    margin: 20px 0px;
-
-    @media (max-width: 450px) {
-      font-size: 20px;
-    }
-  }
 </style>
